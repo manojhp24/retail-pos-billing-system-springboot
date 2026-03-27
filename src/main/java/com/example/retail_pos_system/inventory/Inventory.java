a1c0bf712e3d33710b7ab53a9a0ab30e9607cb0f
Manoj HP <manojhp584@gmail.com>
2026-03-27 13:03:20 +0530

    invetory update

--- src/main/java/com/example/retail_pos_system/inventory/Inventory.java
+++ src/main/java/com/example/retail_pos_system/inventory/Inventory.java
@@ -4,7 +4,14 @@ import java.time.LocalDateTime;
 
 import com.example.retail_pos_system.product.Product;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "inventory")
--- src/main/java/com/example/retail_pos_system/inventory/InventoryRepository.java
+++ src/main/java/com/example/retail_pos_system/inventory/InventoryRepository.java
@@ -7,5 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface InventoryRepository extends JpaRepository<Inventory,Long> {
 	
 	Optional<Inventory> findByProductId(Long productId);
+	
+
+	
 
 }
