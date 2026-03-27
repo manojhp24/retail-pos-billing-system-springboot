4ecb99a322481ddc52399b8304bc0a321f031a4f
Manoj HP <manojhp584@gmail.com>
2026-03-27 13:06:20 +0530

    product updated

--- src/main/java/com/example/retail_pos_system/product/ProductService.java
+++ src/main/java/com/example/retail_pos_system/product/ProductService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.example.retail_pos_system.inventory.InventoryService;
 /**
  * It Handles business logic for product operations & CRUD - operations
  */
